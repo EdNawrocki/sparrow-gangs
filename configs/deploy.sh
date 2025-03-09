1116dfc02f848631a619f65a3ad8cfa2403d10b4
PracticeAccountSimon <simontraub@g.ucla.edu>
2025-03-09 14:46:04 -0700

    Minor change, ignore

--- configs/deploy.sh
+++ configs/deploy.sh
@@ -12,16 +12,16 @@ mvn compile
 mvn package -Dmaven.test.skip=true
 
 # this starts the instance of sparrow that we will register the frontend and a backend to
-java -cp target/sparrow-1.0-SNAPSHOT.jar edu.berkeley.sparrow.daemon.SparrowDaemon -c configs/sparrow_sleep_app.conf
+java -cp target/sparrow-1.0-SNAPSHOT.jar edu.berkeley.sparrow.daemon.SparrowDaemon -c configs/sparrow_sleep_app.conf &
 
 # this starts the instance of sparrow that will only be acting as a node monitor
-java -cp target/sparrow-1.0-SNAPSHOT.jar edu.berkeley.sparrow.daemon.SparrowDaemon -c configs/nm-1.conf
+java -cp target/sparrow-1.0-SNAPSHOT.jar edu.berkeley.sparrow.daemon.SparrowDaemon -c configs/nm-1.conf &
 
 # this will register a backend to nm-1
-java -cp target/sparrow-1.0-SNAPSHOT.jar edu.berkeley.sparrow.examples.SimpleBackend -c configs/backend.conf
+java -cp target/sparrow-1.0-SNAPSHOT.jar edu.berkeley.sparrow.examples.SimpleBackend -c configs/backend.conf &
 
 # this will register a backend to sparrow_sleep_app
-java -cp target/sparrow-1.0-SNAPSHOT.jar edu.berkeley.sparrow.examples.SimpleBackend -c configs/backend-2.conf
+java -cp target/sparrow-1.0-SNAPSHOT.jar edu.berkeley.sparrow.examples.SimpleBackend -c configs/backend-2.conf &
 
 # this will register the frontend to sparrow_sleep_app and job scheduling will begin
 java -cp target/sparrow-1.0-SNAPSHOT.jar edu.berkeley.sparrow.examples.SimpleFrontend -c configs/frontend.conf
